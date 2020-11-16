19b550236a87e44b50a3be8f89169b011ad3e638
rafael.quelho <rafael.quelho@norus.com.br>
2020-11-16 08:56:13 -0300

    classe item

---
+++ design-patterns/Item.cs
@@ -0,0 +1,15 @@
+namespace design_patterns
+{
+    public class Item
+    {
+        public string Nome { get; private set; }
+        public double Valor { get; private set; }
+
+        public Item(string nome, double valor)
+        {
+            this.Nome = nome;
+            this.Valor = valor;
+        }
+
+    }
+}
--- design-patterns/Orcamento.cs
+++ design-patterns/Orcamento.cs
@@ -1,14 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 namespace design_patterns
 {
     public class Orcamento
     {
-        public double Valor {get; private set; }
+        public double Valor { get; private set; }
+        public List<Item> Itens { get; private set; }
 
-        public Orcamento(double valor)
+        public Orcamento(double valor, List<Item> itens)
         {
             this.Valor = valor;
+            this.Itens = itens;
+        }
+
+        public void AdicionaItem(Item item)
+        {
+            Itens.Add(item);
         }
     }
 }
